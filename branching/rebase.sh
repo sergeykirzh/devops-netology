180a39068b3a18f66cf01dbe75e78e7118d3ba76
Sergey Kirzhaev <blynksekir80@gmail.com>
2021-11-02 05:18:18 +0000

    Merge branch 'git-merge'

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+!/bin/bash
 # display command line options
 
 count=1
 for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
+    echo "Next parameter: $param"
     count=$(( $count + 1 ))
 done
 
